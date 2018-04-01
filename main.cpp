8988da7641f87f8af5ac3f95ee41cde6c07ffa76
Oleh Parshenko <jupiterfirepower@gmail.com>
2018-04-01 20:14:03 +0300

    add chrono time for ;oad data and parse in main.cpp

--- main.cpp
+++ main.cpp
@@ -20,9 +20,11 @@
 #include <sstream>
 #include <codecvt>
 #include <clocale>
+#include <chrono>
 #include "Document.h"
 #include "Node.h"
 
+using namespace chrono;
 using vacancy = entity::vacancy;
 using pages = entity::pages;
 using vacinfo = entity::vacinfo;
@@ -37,20 +39,28 @@ int main(int argc, char *argv[])
     std::list<vacancy> vac_list;
 
     Resource::SiteRabota rb;
+    // Get starting timepoint
+    const auto start = high_resolution_clock::now();
+
     auto result1 = rb.searchVacancies();
+
     std::wcout << L"Vacancy Short List Size : "<< result1.size() << std::endl;
     //auto tmplist = first_n(result1, 50);
     auto tmplist = result1;
+
     auto vresult = rb.getVacanciesInfo(tmplist);
+    const auto stop = high_resolution_clock::now();
+
     std::wcout << L"Vacancy Info Extended List Size : "<< vresult.size() << std::endl;
-    std::wcout << std::endl;
+    // Get duration. Substart timepoints to get durarion. 
+    // To cast it to proper unit use duration cast method
+    const auto duration = duration_cast<seconds>(stop - start);
+    std::wcout << "Time taken by function load and parse: " << duration.count() << " seconds" << endl;
+
     rb.writeVacanciesInfoInFile(vresult,"vacinfo.txt");
     std::wcout << L"Vac Info Saved in File : vacinfo.txt" << std::endl;
     rb.writeVacanciesInfoAsHtml(vresult,"vacinfo.html");
     std::wcout << L"Vac Info Saved as HTML in File : vacinfo.html" << std::endl;
 
-    std::wcout << L"Finish press Enter." << std::endl;
-    std::cin.get();
-
-  return 0;
+    return 0;
 }

cfc876d6ecef189bbb910e31b5b9e053a5c8e61f
Oleh Parshenko <jupiterfirepower@gmail.com>
2018-04-02 12:18:33 +0300

    add exeption handling in main

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@
 #include "Document.h"
 #include "Node.h"
 
+using namespace std;
 using namespace chrono;
 using vacancy = entity::vacancy;
 using pages = entity::pages;
@@ -32,35 +33,55 @@ using vacinfo = entity::vacinfo;
 using std::string;
 using std::ostringstream;
 
+template<typename T>
+void printError(T& ex) noexcept(is_base_of<exception, T>::value)
+{
+    std::cerr << ex.what() << endl;
+}
 
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "Russian");
     std::list<vacancy> vac_list;
 
-    Resource::SiteRabota rb;
-    // Get starting timepoint
-    const auto start = high_resolution_clock::now();
+    try
+    {
+	Resource::SiteRabota rb;
+	// Get starting timepoint
+	const auto start = high_resolution_clock::now();
 
-    auto result1 = rb.searchVacancies();
+	auto result = rb.searchVacancies();
 
-    std::wcout << L"Vacancy Short List Size : "<< result1.size() << std::endl;
-    //auto tmplist = first_n(result1, 50);
-    auto tmplist = result1;
+        std::wcout << std::endl << L"Vacancy Short List Size : "<< result.size() << std::endl;
+	//auto tmplist = first_n(result, 50);
+        auto tmplist = result;
 
-    auto vresult = rb.getVacanciesInfo(tmplist);
-    const auto stop = high_resolution_clock::now();
+        auto vresult = rb.getVacanciesInfo(tmplist);
+	const auto stop = high_resolution_clock::now();
 
-    std::wcout << L"Vacancy Info Extended List Size : "<< vresult.size() << std::endl;
-    // Get duration. Substart timepoints to get durarion. 
-    // To cast it to proper unit use duration cast method
-    const auto duration = duration_cast<seconds>(stop - start);
-    std::wcout << "Time taken by function load and parse: " << duration.count() << " seconds" << endl;
+        std::wcout << L"Vacancy Info Extended List Size : "<< vresult.size() << std::endl;
+	// Get duration. Substart timepoints to get durarion. 
+	// To cast it to proper unit use duration cast method
+	const auto duration = duration_cast<seconds>(stop - start);
+	std::wcout << std:: endl << "Time taken by function load and parse: " << duration.count() << " seconds" << std::endl << std::endl;
 
-    rb.writeVacanciesInfoInFile(vresult,"vacinfo.txt");
-    std::wcout << L"Vac Info Saved in File : vacinfo.txt" << std::endl;
-    rb.writeVacanciesInfoAsHtml(vresult,"vacinfo.html");
-    std::wcout << L"Vac Info Saved as HTML in File : vacinfo.html" << std::endl;
+	rb.writeVacanciesInfoInFile(vresult,"vacinfo.txt");
+	std::wcout << L"Vac Info Saved in File : vacinfo.txt" << std::endl;
+        rb.writeVacanciesInfoAsHtml(vresult,"vacinfo.html");
+	std::wcout << L"Vac Info Saved as HTML in File : vacinfo.html" << std::endl;
+    }
+    catch (const runtime_error& ex)
+    {
+	printError(ex);
+    }
+    catch (exception& ex)
+    {
+	printError(ex);
+    }
+    catch (...)
+    {
+	std::cerr << "error ..." << endl;
+    }
 
     return 0;
 }
